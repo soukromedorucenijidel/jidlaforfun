fcc1c2173f73c8960332678eed4dc9026bb77aaf
CreatorOfBiontium <peaceonukraine@gmail.com>
2022-04-24 19:09:57 +0200

    New update
    1.repaire errors, bugs and mistakes
    2. Added new functions

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+
+console.log("If you don´t know the owner, thent don´t visit this website")
+console.log("This website don´t have a virus in ti, but this website is only for personal use")  
+console.log("Pro informace o této webové stránce přejděte na: https://soukromedorucenijidel.github.io/jidlaforfun/informace")  
